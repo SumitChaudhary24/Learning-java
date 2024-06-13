8720675530fab0afea54fd7361eedd11c91ef1f0
Sumit Chaudhary <sumit87459@gmail.com>
2024-06-13 20:42:36 +0530

    Day-1 Hello world and camel case

---
+++ variable.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args) {
+        boolean isPassed = true;
+                byte marks = 29;
+                short count = 3000;
+                int number = 45;
+                long counting = 200000000;
+                float decimals = 0.012326f;
+                double jerseyNumber = 1.323123232d;
+                char Letter = 'B';
+        System.out.println(isPassed);
+        System.out.println(marks);
+        System.out.println(count);
+        System.out.println(number);
+        System.out.println(counting);
+        System.out.println(decimals);
+        System.out.println(jerseyNumber);
+        System.out.println(Letter);
+        long newCount = count;
+        System.out.println(newCount);
+        byte newNumber = (byte)number; //tyecasting
+        System.out.println(newNumber);
