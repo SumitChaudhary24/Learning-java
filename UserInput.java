625e9a5e9503e60af386680313187e1746243a0b
Sumit Chaudhary <sumit87459@gmail.com>
2024-06-14 17:22:02 +0530

    Day-2 Types of Operators

---
+++ UserInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        System.out.println("Your age is "+age);
+        int firstNumber = sc.nextInt();
+        int secondNumber = sc.nextInt();
+        int result = firstNumber+secondNumber;
+        System.out.println(result);
+        sc.close();
+    }
+}
