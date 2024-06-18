343a947e0b38f9060162c4e41d6243a045e696a0
Sumit Chaudhary <sumit87459@gmail.com>
2024-06-18 16:41:59 +0530

    Day-5 Multi-Dimensional Array

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,35 @@
+public class Multidimensionalarray {
+    public static void main(String[] args) {
+        int marks[][]= {
+                {97,23,43},
+                {15,67,76},
+                {75,89,76},
+                {87,45,65}
+        };
+        System.out.println(marks[0][1]);
+        System.out.println(marks[0][0]);
+        System.out.println(marks[0][2]);
+        System.out.println(marks[1][0]);
+        System.out.println(marks[1][2]);
+//        int marks[][]= new int[4][3];
+//
+//        marks[0][0]=98;
+//        marks[0][1]=98;
+//        marks[0][2]=98;
+//
+//        marks[1][0]=98;
+//        marks[1][1]=98;
+//        marks[1][2]=98;
+//
+//        marks[2][0]=98;
+//        marks[2][1]=98;
+//        marks[2][2]=98;
+//
+//        marks[3][0]=98;
+//        marks[3][1]=98;
+//        marks[3][2]=98;
+
+    }
+
+
+}
