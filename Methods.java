4c91f97e3c8128448cdbabe34bef08635be714b2
Sumit Chaudhary <sumit87459@gmail.com>
2024-06-19 16:55:12 +0530

    Day-6 Methods

---
+++ Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+    public static void main(String[] args) {
+      greet();
+        System.out.println(1);
+        System.out.println(2);
+    }                                 //Calling a method
+    static void greet() {
+        System.out.println(3);
+        System.out.println("Hello World");
+        System.out.println(4);
+    }
+}
