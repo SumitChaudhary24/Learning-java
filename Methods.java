067701a57b83be2a5b43e7dc385437338f26da43
Sumit Chaudhary <sumit87459@gmail.com>
2024-06-20 12:03:31 +0530

    Day-6 Methods

--- Methods.java
+++ Methods.java
@@ -1,12 +1,19 @@
 public class Methods {
     public static void main(String[] args) {
-      greet();
-        System.out.println(1);
-        System.out.println(2);
+//      greet();
+//        System.out.println(1);
+//        System.out.println(2);
+        average(4,6,false);
     }                                 //Calling a method
     static void greet() {
         System.out.println(3);
         System.out.println("Hello World");
         System.out.println(4);
+    }
+
+    public static void average(int a, int b,boolean shouldAverage) {
+        int avg = (a+b)/2;
+        System.out.println("The Average is: "+avg);
+
     }
 }
